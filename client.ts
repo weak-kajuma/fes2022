9d91d4ffd216d719107783646ba64709e6fe6118
weak-kajuma <kazuma826826@gmail.com>
2022-09-16 16:36:06 +0900

    create page

---
+++ client.ts
@@ -0,0 +1,6 @@
+import { createClient } from 'microcms-js-sdk'
+
+export const client = createClient({
+    serviceDomain: 'tepic',
+    apiKey: '7b74f4c20f2543829b99935b445554c9e57c'
+})
---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import type { GetServerSideProps,NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.scss'
+import { client } from '../client'
+
+interface product {
+  id: string
+  createdAt: string
+  updatedAt: string
+  url: string
+  title: string
+  author: string
+  image: string
+}
+
+const shuffleArray = (array: any) => {
+  const cloneArray = [...array];
+
+  const result = cloneArray.reduce((_,cur,idx) => {
+    let rand = Math.floor(Math.random() * (idx + 1));
+    cloneArray[idx] = cloneArray[rand]
+    cloneArray[rand] = cur;
+    return cloneArray
+  })
+
+  return result;
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await client.get({endpoint: 'fes2022',queries: {offset: 0,limit: 100}})
+  const products = res.contents
+  console.log(products)
+  return {
+    props: {
+      data: products
+    }
+  }
+}
+
+const Home: NextPage<any> = ({ data }) => {
+  return (
+    <div>
+      <h1 className={styles.title}>作品一覧</h1>
+      <div className={styles.main}>
+        {data.map((product: product) => {
+          return (
+            <div key={product.id}>
+              <a href={product.url}>
+                <h2>{product.title}</h2>
+                <p className={styles.author}>Made by {product.author}</p>
+              </a>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Home
